36815338eaad60e2603859140e3b432d3e64e9b2
Tarcisio Gruppi <txgruppi@gmail.com>
2012-05-19 21:29:28 -0300

    Changed hashbang to /usr/bin/env node

--- bin/beautifier.js
+++ bin/beautifier.js
@@ -1,3 +1,3 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 var e=require("../lib/beautify"),nopt=require("nopt"),fs=require("fs"),parsed=nopt();function die(b){console.warn(b);console.warn("Usage: "+process.argv[1]+" file.{js,css,html} ...");process.exit(1)}
 function beautifyFile(b){var c=b.split(".");fs.readFile(b,function(b,a){if(b)throw b;239===a[0]&&(187===a[1]&&191===a[2])&&(a=a.slice(3));a=a.toString("utf8");switch(c[c.length-1]){case "js":beauty=e.js_beautify;break;case "css":beauty=e.css_beautify;break;case "html":beauty=e.html_beautify;break;default:die("invalid file format")}console.log(beauty(a))})}parsed.argv.remain.length||die("No files specified.");parsed.argv.remain.forEach(beautifyFile);
